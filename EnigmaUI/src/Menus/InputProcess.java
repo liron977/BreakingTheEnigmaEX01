8ab923140b92ba08d0789d7fac12024f1709026e
chenpakman <chen36361@gmail.com>
2022-08-05 20:45:04 +0300

    changes

---
+++ EnigmaUI/src/Menus/InputProcess.java
@@ -0,0 +1,34 @@
+package Menus;
+
+import Console.UserConsole;
+
+import java.util.Scanner;
+
+public class InputProcess implements MenuManager{
+    String userInput;
+    public UserConsole userConsole;
+    public InputProcess(UserConsole userConsole){
+        this.userConsole=userConsole;
+
+    }
+    @Override
+    public void execution() {
+        boolean isValidInputProcess=false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the string to process");
+        userInput = scanner.nextLine();
+        while(!isValidInputProcess){
+
+           isValidInputProcess=userConsole.isUserStringToProcessIsValid(userInput.toUpperCase());
+           if(isValidInputProcess) {
+               System.out.println("The converted string is: "+userConsole.getConvertedString(userInput.toUpperCase()));
+
+           }
+          else{
+               userInput = scanner.nextLine();
+           }
+
+        }
+
+    }
+}
